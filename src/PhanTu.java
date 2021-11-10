42cdeb4e6037e8cbfbef1c72f642cc3f658d09a5
Tran Gia Bao <trangiabao3126@gmail.com>
2021-11-10 21:09:44 +0700

    Bai tap

--- src/PhanTu.java
+++ src/PhanTu.java
@@ -1,7 +1,43 @@
-public class PhanTu<T> {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class PhanTu<T extends Comparable<T>>{
     private T t;
 
-    public boolean isGreaterThan(T a) {
+    public boolean isGreaterThan(T other) {
+        if (t.compareTo(other) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public static void main(String[] args) {
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+
+        try {
+            File input = new File(args[0]);
+            Scanner readFile = new Scanner(input);
+            while (readFile.hasNextInt()) {
+                int a = readFile.nextInt();
+                integerArrayList.add(a);
+            }
+            Collections.sort(integerArrayList);
+
+            File output = new File(args[1]);
+            FileWriter fileWriter = new FileWriter(output, false);
+            PrintWriter printWriter = new PrintWriter(fileWriter, true);
+            String printedLine = "";
+            for (Integer integer : integerArrayList) {
+                printedLine += integer + " ";
+            }
+            printWriter.println(printedLine);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
